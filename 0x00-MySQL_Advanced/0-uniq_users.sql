a3bdc7389f690c936184421ccd72dddaffec7b83
Brian <chumobrian@gmail.com>
2024-04-25 13:35:06 +0000

    feat: Add a SQL script that creates a table users with id, email and name columns

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,11 @@
+-- creates a table users with these attributes:
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+-- If the table already exists, your script should not fail
+-- Your script can be executed on any database
+CREATE TABLE IF NOT EXISTS users(
+	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	email VARCHAR(255) UNIQUE NOT NULL,
+	name VARCHAR(255) NOT NULL
+);
