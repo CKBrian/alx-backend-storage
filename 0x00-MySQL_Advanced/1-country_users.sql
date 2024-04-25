1a6ce5723bcf77223a270ebdc4f99de3e5d56ef8
Brian <chumobrian@gmail.com>
2024-04-25 17:17:00 +0300

    feat: Add a SQL script that creates a table users with id, email and name columns

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,13 @@
+-- creates a table users with these attributes:
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+-- country, enumeration of countries: US, CO and TN, never null (= default will be the first element of the enumeration, here US)
+-- If the table already exists, your script should not fail
+-- Your script can be executed on any database
+CREATE TABLE IF NOT EXISTS users(
+	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	email VARCHAR(255) UNIQUE NOT NULL,
+	name VARCHAR(255) NOT NULL,
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+);
