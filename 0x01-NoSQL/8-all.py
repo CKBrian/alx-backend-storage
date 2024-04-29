5b39836328156a96f7f7b38344116558b333b880
Brian <chumobrian@gmail.com>
2024-04-29 22:31:53 +0300

    Feat: Add a function that lists all documents in a collection

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """Defines a module with a function that lists all
     documents in a collection in MongoDb"""
 
-import pymongo
+from typing import List
 
 
-def list_all(mongo_collection):
+def list_all(mongo_collection: pymongo.collection.Collection) -> List[dict]:
     """lists all documents in a collection in MongoDb"""
     cursor = mongo_collection.find({})
     all_docs = [doc for doc in cursor]
