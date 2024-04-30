7d00ab4277f9ca81ff188910167138fd9afa10c7
Brian <chumobrian@gmail.com>
2024-04-30 22:47:49 +0300

    Feat: Add a function that returns a collection stats based on filters

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Defines a module that provides some stats
+   about Nginx logs stored in MongoDB"""
+
+from pymongo import MongoClient
+get_stats = __import__('12-log_stats').get_stats
+
+
+def get_ip_stats(nginx_coll):
+    """returns stats about Nginx logs stored in MongoDB"""
+    pipeline = [
+        {
+            "$group": {
+                "_id": "$ip",
+                "count": {"$sum": 1}
+            }
+        },
+        {
+            "$sort": {"count": -1}
+        },
+        {
+            "$limit": 10
+        }
+    ]
+    cursor = nginx_coll.aggregate(pipeline)
+    return "\n".join(f"\t{doc.get('_id')}: {doc.get('count')}" for doc in cursor)
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+    print(get_stats(nginx_collection))
+    print("IPs:")
+    print(get_ip_stats(nginx_collection))
