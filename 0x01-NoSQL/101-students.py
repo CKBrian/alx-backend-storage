e60484877d383fdd330a8288746d6ab1934974a7
Brian <chumobrian@gmail.com>
2024-04-30 21:36:57 +0300

    Feat: Add a function that returns all students sorted by average score

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" Defines a module with a function that returns all students
+    sorted by average score
+"""
+
+
+def top_students(mongo_collection):
+    """
+    mongo_collection will be the pymongo collection object
+    The top must be ordered
+    The average score must be part of each item returns with key = averageScore
+    """
+    pipeline = [
+        {
+            "$project": {
+                "name": 1,
+                "$averageScore": {"$avg": "topics.score"}
+            }
+        },
+        {
+            "$sort": {"averageScore": 1}
+        }
+    ]
+    return mongo_collection.aggregate(pipeline)
