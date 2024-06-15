99182963bb921e3a58bef6dde1020be2e57ab5bc
Brian <chumobrian@gmail.com>
2024-06-15 22:32:29 +0300

    feat: add  an SQL script that creates an index idx_name_first on the table names and the first letter of name.

---
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -0,0 +1,5 @@
+
+-- creates an index idx_name_first on the table names and the first letter of name.
+-- ALTER TABLE names ADD first_letter VARCHAR(1);
+-- UPDATE names SET first_letter = LEFT(name, 1);
+CREATE INDEX idx_name_first ON names(LEFT(name, 1), score);
